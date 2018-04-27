d0ab72990000461195f48511a158131404c0e65a
Nelio Alves <acenelio@gmail.com>
2018-04-27 12:11:42 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.nelioalves.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userReposiroty;
+	
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		userReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
