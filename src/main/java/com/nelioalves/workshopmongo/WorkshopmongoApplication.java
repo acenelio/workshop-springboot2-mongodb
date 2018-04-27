48e17466e2db690c9d3cd8f7e93715df3c7c3d90
Nelio Alves <acenelio@gmail.com>
2018-04-27 10:27:25 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,12 @@
+package com.nelioalves.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+}
